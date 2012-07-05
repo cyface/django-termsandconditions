d2dc88985d1823e718d3daecab5127055f1b6979
cyface <tim@cyface.com>
2012-07-05 16:01:09 -0600

    Removed last Gondor vestige.

--- termsandconditions_demo/settings.py
+++ termsandconditions_demo/settings.py
@@ -12,9 +12,3 @@ try:
 except ImportError:
     from settings_local_template import * #@UnusedWildImport
     #print("ERROR: You need to rename settings_local_template.py to settings_local.py and customize it.")
-    
-# Import Gondor auto-generated local settings if they exist.
-try:
-    from local_settings import *
-except ImportError:
-    pass
