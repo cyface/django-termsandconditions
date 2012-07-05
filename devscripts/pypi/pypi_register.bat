e7e7779c10c6e4d64bafe07104988c7ed3138ffc
cyface <tim@cyface.com>
2012-07-05 14:25:31 -0600

    Added devscripts.

---
+++ devscripts/pypi/pypi_register.bat
@@ -0,0 +1,3 @@
+REM This script uploads this package to PyPi - it should be run from the same dir that setup.py is in
+
+python setup.py register
---
+++ devscripts/pypi/pypi_register.sh
@@ -0,0 +1,3 @@
+# This script uploads this package to PyPi - it should be run from the same dir that setup.py is in
+
+python setup.py register
