90c6be974f5283e2cca417e0d61eae44e9c4a84d
cyface <tim@cyface.com>
2012-07-06 07:45:23 -0600

    Working PyPi upload process.

--- devscripts/pypi/pypi_register.bat
+++
@@ -1,3 +0,0 @@
-REM This script uploads this package to PyPi - it should be run from the same dir that setup.py is in
-
-python setup.py register
--- devscripts/pypi/pypi_sdist.bat
+++
@@ -1,3 +0,0 @@
-REM This script creates source distribution - it should be run from the same dir that setup.py is in
-
-python setup.py sdist
