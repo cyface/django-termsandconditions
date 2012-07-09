599d59c60d70e6febd0302507764883411bcb5b9
cyface <tim@cyface.com>
2012-07-09 08:02:17 -0600

    Added PIP_DOWNLOAD_CACHE to windows devscript.

--- devscripts/virtualenv/setup_dev_env.bat
+++ devscripts/virtualenv/setup_dev_env.bat
@@ -3,6 +3,8 @@ REM Assuming you have python installed, you can install virtualenv using the com
 REM curl -O https://raw.github.com/pypa/virtualenv/master/virtualenv.py
 REM This should be run from the project directory, not inside the project dir
 
+SET PIP_DOWNLOAD_CACHE=..\pip_download_cache
+
 virtualenv --system-site-packages django-termsandconditions-env
 call django-termsandconditions-env\Scripts\activate.bat
 pip install -r termsandconditions_demo/requirements_dev.txt
