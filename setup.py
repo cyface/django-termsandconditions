d39b006d84f48e3c6bd94071e12378743d4dc961
adrian <adrian.bohdanowicz@wilabs.com>
2015-11-24 14:48:46 +0100

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="django-termsandconditions",
-    version="0.1.6",
+    version="0.1.7",
     url='http://timlwhite.com',
     license='BSD',
     description="django-termsandconditions enables users to accept terms and conditions of a site.",
