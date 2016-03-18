b51120dc3065b348f2fdc83becb65ccda58dbecf
Tim White <tim@cyface.com>
2016-03-18 08:53:28 -0600

    Pylint cleanup

--- termsandconditions/test_models.py
+++ termsandconditions/test_models.py
@@ -4,8 +4,10 @@ from termsandconditions.models import TermsAndConditions, DEFAULT_TERMS_SLUG
 
 
 class TermsAndConditionsTestCase(TestCase):
+    """Tests Models for T&C"""
 
     def setUp(self):
+        """Set Up T&C Model Tests"""
         self.terms_1 = TermsAndConditions.objects.create(
             name='terms_1',
             date_active=timezone.now()
@@ -15,11 +17,11 @@ class TermsAndConditionsTestCase(TestCase):
             date_active=None
         )
 
-    def test_termsandconditions_default_slug(self):
+    def test_tnc_default_slug(self):
         """test if default slug is used"""
         self.assertEqual(self.terms_1.slug, DEFAULT_TERMS_SLUG)
 
-    def test_termsandconditions_get_active(self):
+    def test_tnc_get_active(self):
         """test if right terms are active"""
         active = TermsAndConditions.get_active()
         self.assertEqual(active.name, self.terms_1.name)
