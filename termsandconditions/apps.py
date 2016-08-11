5c31743859db31cfafb0966f8c82f19b4d11393a
cyface <tim@cyface.com>
2016-08-11 12:45:29 -0600

    Scrutinizer fix.

--- termsandconditions/apps.py
+++ termsandconditions/apps.py
@@ -4,5 +4,6 @@ from django.apps import AppConfig
 
 
 class TermsAndConditionsConfig(AppConfig):
+    """App config for TermsandConditions"""
     name = 'termsandconditions'
     verbose_name = "Terms and Conditions"
