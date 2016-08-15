0100c5fefbaf6ae9341bdc5377c31858c6ad81a0
Tim White <tim@cyface.com>
2016-08-15 11:18:03 -0600

    Update pipeline.py

--- termsandconditions/pipeline.py
+++ termsandconditions/pipeline.py
@@ -9,7 +9,6 @@ except ImportError:
 from .models import TermsAndConditions
 from django.http import HttpResponseRedirect, QueryDict
 from django.conf import settings
-from django.core.urlresolvers import reverse
 import logging
 
 ACCEPT_TERMS_PATH = getattr(settings, 'ACCEPT_TERMS_PATH', '/terms/accept/')
