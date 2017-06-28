3180db6fe969712c7b3f28bdac27b38015acbb8a
cyface <tim@cyface.com>
2017-06-28 07:28:01 -0600

    Removed unused Http404 import.

--- termsandconditions/models.py
+++ termsandconditions/models.py
@@ -5,7 +5,6 @@ from collections import OrderedDict
 
 from django.db import models, utils
 from django.conf import settings
-from django.http import Http404
 from django.utils import timezone
 import logging
 
