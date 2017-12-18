1a4c229c90c8afb49a882e139a526b21942ae57e
cyface <tim@cyface.com>
2017-12-18 10:47:10 -0700

    Tweaking setup.py to run tests.

--- termsandconditions_demo/run_tests.py
+++ termsandconditions_demo/run_tests.py
@@ -10,11 +10,11 @@ from django.test.utils import get_runner
 from django.conf import settings
 
 
-def runtests():
+def run_tests():
     test_runner = get_runner(settings)
     failures = test_runner([], verbosity=1, interactive=True)
     sys.exit(failures)
 
 
 if __name__ == '__main__':
-    runtests()
+    run_tests()
