165da8e46845b1e32bba04c8b4202540b28cedfe
LampLight <mamosiko@gmail.com>
2018-03-19 21:18:25 +0500

    Removed unneeded translations for fieldnames in admin.py and generated locales for English and Russian language. Other locales can be generated with django-admin makemessages -l {locale_code} in module root.

--- termsandconditions/admin.py
+++ termsandconditions/admin.py
@@ -9,16 +9,16 @@ from .models import TermsAndConditions, UserTermsAndConditions
 
 class TermsAndConditionsAdmin(admin.ModelAdmin):
     """Sets up the custom Terms and Conditions admin display"""
-    list_display = (_('slug'), _('name'), _('date_active'), _('version_number'),)
+    list_display = ('slug', 'name', 'date_active', 'version_number',)
     verbose_name = _("Terms and Conditions")
 
 
 class UserTermsAndConditionsAdmin(admin.ModelAdmin):
     """Sets up the custom User Terms and Conditions admin display"""
     # fields = ('terms', 'user', 'date_accepted', 'ip_address',)
-    readonly_fields = (_('date_accepted'),)
-    list_display = (_('terms'), _('user'), _('date_accepted'), _('ip_address'),)
-    date_hierarchy = _('date_accepted')
+    readonly_fields = ('date_accepted',)
+    list_display = ('terms', 'user', 'date_accepted', 'ip_address',)
+    date_hierarchy = 'date_accepted'
     list_select_related = True
 
 
