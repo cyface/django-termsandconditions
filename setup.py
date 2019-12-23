0df4e23e663ce1a905f6e58776c93bf9f51460e9
TIm White <twhite@fool.com>
2019-12-23 08:44:51 -0700

    Update requirements.

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Topic :: Internet :: WWW/HTTP",
     ],
 )
