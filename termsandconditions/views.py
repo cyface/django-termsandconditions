2f8abe4f125a8d2844911bdf909b414a74e33256
Tim White <tim@cyface.com>
2020-02-16 06:57:19 -0700

    Working on handling no terms defined better.

--- termsandconditions/views.py
+++ termsandconditions/views.py
@@ -9,7 +9,7 @@ from .models import TermsAndConditions, UserTermsAndConditions
 from django.conf import settings
 from django.core.mail import send_mail
 from django.contrib import messages
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, Http404
 from django.utils.translation import gettext as _
 from django.views.generic import DetailView, CreateView, FormView
 from django.template.loader import get_template
@@ -57,6 +57,9 @@ class TermsView(DetailView, GetTermsViewMixin):
     def get_context_data(self, **kwargs):
         """Pass additional context data"""
         context = super(TermsView, self).get_context_data(**kwargs)
+        if context["terms_list"][0] is None:
+            # TODO: Raise Terms No Defined Instead
+            raise Http404
         context["terms_base_template"] = getattr(
             settings, "TERMS_BASE_TEMPLATE", DEFAULT_TERMS_BASE_TEMPLATE
         )
