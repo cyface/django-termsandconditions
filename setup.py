c5f983da3c93753401a89a4c10c9e05f44110be0
Tim White <twhite@fool.com>
2020-02-17 06:30:51 -0700

    Version 2.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="django-termsandconditions",
-    version="2.0.2",
+    version="2.0.3",
     url="https://github.com/cyface/django-termsandconditions",
     license="BSD",
     description="django-termsandconditions is a Django app that enables users to accept terms and conditions of a site.",
